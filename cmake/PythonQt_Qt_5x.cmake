cea9bee69b020e34c0b69f967ffaa884dd56ab88
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2016-01-01 10:21:41 +0100

    add missing files

---
+++ cmake/PythonQt_Qt_5x.cmake
@@ -0,0 +1,19 @@
+find_package(Qt5Core REQUIRED)
+
+# aliases
+macro(qt_use_modules)
+  qt5_use_modules(${ARGN})
+endmacro()
+
+macro(qt_wrap_cpp)
+  qt5_wrap_cpp(${ARGN})
+endmacro()
+
+macro(qt_add_resources)
+  qt5_add_resources(${ARGN})
+endmacro()
+
+# version
+set(QT_VERSION_MAJOR ${Qt5Core_VERSION_MAJOR})
+set(QT_VERSION_MINOR ${Qt5Core_VERSION_MINOR})
+set(QT_VERSION_PATCH ${Qt5Core_VERSION_PATCH})
